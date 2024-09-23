66391a057360c5fd0503ca5cf856cb27eaf7d9d0
{dohisaneaki} <dohistudy@gmail.com>
2024-09-23 20:34:39 +0900

    9/23（CSS調整その２）

--- resources/views/users/test.blade.php
+++ resources/views/users/test.blade.php
@@ -1,31 +1,43 @@
 
 <!DOCTYPE html>
-<html lang="ja">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ログイン</title>
+    <link rel="stylesheet" href="{{ asset('css/sane.css') }}">
 </head>
 <body>
+    <div class="container">
+    <div>
     <h1>ログイン</h1>
+    </div>
+
     <form method="POST" action="{{ route('users.login') }}">
         @csrf
+        <div class="description">
+            <div class = "form">
         <div>
-            <label for="email">メールアドレス:</label>
+            <label for="email">メールアドレス</label><br>
             <input type="email" id="email" name="email" value="{{ old('email') }}" required>
             @error('email')
                 <div>{{ $message }}</div>
             @enderror
         </div>
         <div>
-            <label for="password">パスワード:</label>
+            <label for="password">パスワード</label><br>
             <input type="password" id="password" name="password" required>
             @error('password')
                 <div>{{ $message }}</div>
             @enderror
         </div>
-        <div>
-            <button type="submit">ログイン</button>
+            </div>
+        </div>
+        <div class="links">
+            <div class="left-link">
+                <button class="button-link">ログイン</button>
+            </div>
+            <div class="right-link">
+                <input type="button" class="button-back" value="戻る" onclick= "history.back()">
+            </div>
         </div>
     </form>
 </body>
